4a946408e0f789ced023f7dc0e78131de4ae0a35
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-02-02 06:07:46 +0000

    Variable declaration

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 /* Stewart Sheppard
    COSC 1436-57002
    Text Editor: Repl.it
-
+   This is a program to work with the various arithmetic operators in C++ and conditions.  User will enter in 4 numbers and receive in return the additon, subtaction, multiplication and division of the first two numbers, then the remainder of the third and forth numbers.
 */
+using namespace std;
+
 #include <iostream>
 
+
+double = number1, number2, addition,subtraction, multiplication, division; 
+int = number3, number4, modulus;
+
+//Input - ask user to input numbers.
 int main() {
   std::cout << "Hello World!\n";
 }
